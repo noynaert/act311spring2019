ce6cb8b43d761913dcfbaf29f25e15b2edc22922
bomeara <bomeara@missouriwestern.edu>
2019-01-27 21:41:51 -0600

    trying something new

--- Week_02_--_Introduction_to_JavaScript/multiply.js
+++ Week_02_--_Introduction_to_JavaScript/multiply.js
@@ -1,4 +1,10 @@
-function myFunction (multiplicand, multiplier) {
-	return multiplicand * multiplier;
-	}
-document.getElementById("Times").innerHTML =myFunction;
+function multiply () {
+    let x= document.getElementById ("multiplicand").value;
+    console.log ("multiplicand is" + x);
+    let y=document.getElementById ("multiplier").value;
+    console.log ("multiplier is" + y);
+    let z= x * y;
+    document.getElementById ("showProduct").innerHTML=z;
+    
+    
+}
